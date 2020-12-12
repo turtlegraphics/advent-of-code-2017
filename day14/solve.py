6f1445a220629ea5f1a9c08d74e8cf25b165afc8
Bryan Clair <bryan@slu.edu>
2020-12-12 02:44:14 -0600

    So close, but total failure.

--- day14/solve.py
+++ day14/solve.py
@@ -6,6 +6,8 @@
 #
 import sys
 import os
+import itertools
+import string
 sys.path.append("..")
 import aocutils
 from knothash import knothash
@@ -46,6 +48,109 @@ def part1(hashes):
 
     return used
 
-hashes = makehashes(key)
+def display(hashes):
+    for hash in hashes:
+        binary = ''
+        for c in list(hash):
+            binary += format(int(c,16),"04b")
+        bits = [c == '1' for c in list(binary)]
 
+        row = ''
+        for b in bits:
+            row += '#' if b else '.'
+        print row
+
+def showregions(rows):
+    for row in rows:
+        out = ''
+        for r in row:
+            if r:
+                out += str(r)
+            else:
+                out += '.'
+        print out
+
+class Region:
+    newid = itertools.count().next
+    symbols = '123456789' + string.ascii_letters
+    def __init__(self):
+        self.id  = Region.newid()
+        self.subs = []  # track subregions
+
+    def __str__(self):
+        return Region.symbols[self.id % len(Region.symbols)]
+
+    def set_id(self,val):
+        self.id = val
+        for sub in self.subs:
+            sub.set_id(val)
+
+    def join(self,other):
+        if self.id < other.id:
+#            print str(self) + ' eating ' + str(other)
+            self.subs.append(other)
+            other.set_id(self.id)
+        else:
+#            print str(other) + ' eating ' + str(self)
+            other.subs.append(self)
+            self.set_id(other.id)
+            
+
+def part2(hashes):
+    oldrow = [None]*128
+    rows = []
+
+    for hash in hashes:
+        binary = ''
+        for c in list(hash):
+            binary += format(int(c,16),"04b")
+        bits = [c == '1' for c in list(binary)]
+
+        row = []
+        for col in range(128):
+            if not bits[col]:
+                # bit is off, no region here
+                row.append(None)
+                continue
+
+            # bit is on
+            above = (oldrow[col] != None)
+            left = False if (col == 0) else bits[col-1]
+
+            if (not left and not above):
+                row.append(Region()) # new region begins
+                continue
+
+            if (not left and above):
+                row.append(oldrow[col]) # connect above
+                continue
+
+            if (left and not above):
+                row.append(row[col-1]) # connect to left
+                continue
+
+            assert(left and above)
+            if oldrow[col-1] or (row[col-1].id == oldrow[col].id):
+                # above and left already joined
+                if (row[col-1].id != oldrow[col].id):
+                    rows.append(row)
+                    showregions(rows)
+                assert(row[col-1].id == oldrow[col].id)
+            else:
+                # join above and left regions
+                oldrow[col].join(row[col-1])
+
+            # connect to above and left
+            row.append(oldrow[col])
+
+        # row complete
+        rows.append(row)
+        oldrow = row
+
+    return rows
+
+hashes = makehashes(key)
 print 'Part 1:',part1(hashes)
+
+rows = part2(hashes)
+print rows
