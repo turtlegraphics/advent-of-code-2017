28929c78af3039c6c269e07691f8553f3b0efbc0
Bryan Clair <bryan@slu.edu>
2020-12-12 00:58:46 -0600

    Setup for part 2. Save hashes for speed. Make test input.

--- day14/solve.py
+++ day14/solve.py
@@ -5,6 +5,7 @@
 # Day 14
 #
 import sys
+import os
 sys.path.append("..")
 import aocutils
 from knothash import knothash
@@ -15,18 +16,36 @@ inputlines = [x.strip() for x in open(args.file).readlines()]
 
 key = inputlines[0]
 
-bitson = {
-    '0':0,'1':1,'2':1,'3':2,
-    '4':1,'5':2,'6':2,'7':3,
-    '8':1,'9':2,'a':2,'b':3,
-    'c':2,'d':3,'e':3,'f':4
-    }
+def makehashes(key):
+    # get hashes from file for speed
+    hashfile = 'hashes-'+key+'.txt'
+    if os.path.isfile(hashfile):
+        return [x.strip() for x in open(hashfile).readlines()]
 
-used = 0
-for row in range(128):
-    text = key + '-' + str(row)
-    hash = knothash(text)
-    for c in list(hash):
-        used += bitson[c]
+    # generate hashes, print them for user to copy into file
+    h = []
+    for row in range(128):
+        text = key + '-' + str(row)
+        x = knothash(text)
+        print x
+        h.append(x)
+    return h
 
-print 'Part 1:',used
+def part1(hashes):
+    bitson = {
+        '0':0,'1':1,'2':1,'3':2,
+        '4':1,'5':2,'6':2,'7':3,
+        '8':1,'9':2,'a':2,'b':3,
+        'c':2,'d':3,'e':3,'f':4
+        }
+
+    used = 0
+    for hash in hashes:
+        for c in list(hash):
+            used += bitson[c]
+
+    return used
+
+hashes = makehashes(key)
+
+print 'Part 1:',part1(hashes)
