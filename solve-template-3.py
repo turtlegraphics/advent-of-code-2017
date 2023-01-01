af61337bec226f93ae78c5af7ad78db5409fbea7
Bryan Clair <bryan@slu.edu>
2022-12-31 19:54:10 -0600

    May as well use 2022 aocutils

--- solve-template-3.py
+++ solve-template-3.py
@@ -6,38 +6,9 @@
 #
 
 # Picking up again some years later, switching to Python3
-
-from argparse import ArgumentParser
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument("-v", "--verbose",
-                        action = "count",
-                        dest = "verbose",
-                        default = 1,
-                        help = "Set verbosity level (-v, -vv, -vvv,...)")
-    parser.add_argument("-q", "--quiet",
-                        action = "store_const",
-                        const = 0,
-                        dest = "verbose",
-                        help = "Suppress output.")
-    
-    parser.add_argument("-p", "--part",
-                        action="store",
-                        dest = "part",
-                        default = 1,
-                        type = int,
-                        help = "Which part of the problem to solve (1 or 2)")
-    
-    parser.add_argument("file",
-                        nargs = "?",
-                        default = "input.txt",
-                        help = "Problem input file (optional).")
-    args = parser.parse_args()
-    if args.verbose > 2:
-        print(args)
-
-    return args
-
+import sys
+sys.path.append('../../Advent 2022')
+from aocutils import *
 
 args = parse_args()
 
